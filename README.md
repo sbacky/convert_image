1e0be572d85e7d3414b1d734eb9df730642d26f7
Neil <nhc.crum@gmail.com>
2020-12-10 23:34:05 -0700

    add file .gitignore, README.md, scripts/converter.py.

---
+++ README.md
@@ -0,0 +1,82 @@
+# **Image-Converter**
+The purpose of this script is to reformat, rotate and, resize images to the correct format, orientation and, size. This will be accomplished by using the **Python Imaging Library (PIL)** to modify and resave the images.
+
+## <a name="head1"></a>**Contents**
+* [Planning](#head2)
+* [Variables](#head3)
+
+
+## <a name="head2"></a>**Planning**
+The script has to take the directory containing the images as a parameter and go through each image in the directory, converting them to the correct format, orientation and, size. Once converted, the images need to be saved to the directory for finished images.
+
+Image module from the PIL library should provide the necessary functions to process the images correctly. To install the PIL library on your device, use:
+```
+MacOSX, WindowsOS ---> pip3 install pillow
+Linux distr. ---> sudo apt install python3-pil
+```
+
+To import the Image module from the PIL library:
+```python
+from PIL import Image
+```
+
+To resize an image using Image:
+```python
+from PIL import Image
+im = Image("example.jpg")
+new_im = im.resize((640,480))
+new_im.save("example_resized.jpg")
+```
+
+To rotate an image using Image:
+```python
+from PIL import Image
+im = Image("example.jpg")
+new_im = im.rotate(90)
+new_im.save("example_rotated.jpg")
+```
+
+Pass parameters as a system environment variable and use sys.argv(1).split("|") to catch them, then split and save them into separate variables. The variables that are going to be used are: dir, newDir, size, rotate, extension. Make sure to import system module first.
+```python
+import sys
+dir, newDir, size, rotate, extension = sys.argv[1].split("|")
+```
+
+Back to [Contents](#head1)
+
+## <a name="head3"></a>**Variables**
+Variables should be passed as one parameter in the shell as: **converter.py dir|newDir|size|rotate|ext**. There should be no spaces between "|"s and the variables to ensure they get split properly. It is also important to pass the variables in the order they appear so they get saved to the correct variables in the script.
+
+[dir](#head31)|[newDir](#head31)|[size](#head32)|[rotate](#head33)|[ext](#head34)
+
+Back to [Contents](#head1)
+
+### <a name="head31"></a>dir and newDir
+dir and newDir should both be directories. dir is the directory that currently has the images you wish to process. newDir is the directory you wish to save the new processed images to. Error status codes of 1, 10, 11, and 101 are linked to directory issues. Status codes of 1 and 101 can be linked to directory issues but can also be related to size issues [(see below)](#head32).
+
+Example Directories:
+```
+WindowsOS ---> C:\\foo\\bar\\foobar
+MacOS, Linux ---> /foo/bar/foobar
+```
+Back to [Contents](#head1)
+
+### <a name="head32"></a>size
+Desired size of the image should be passed as number1,number2. The number1 and number2 should represent the 'width' x 'height' of the desired size of the image. They will be split by using .split(",") and saved as a tuple. Error status codes of 1, 20, 21, and 101 are linked to size related issues. Status codes of 1 and 101 can be linked to size issues but can also be related to directory issues [(see above)](#head31).
+
+Example size:
+```
+number1,number2 ---> 'width' x 'height'
+640,480 ---> 640 x 480
+```
+Back to [Contents](#head1)
+
+### <a name="head33"></a>rotate
+
+
+Back to [Contents](#head1)
+
+### <a name="head34"></a>ext
+
+
+Back to [Contents](#head1)
---
+++ scripts/converter.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import os, sys
+
+def convert(dir, newDir, size, rotate, extension):
+    images = os.listdir(dir)
+    for image in images:
+
+        print("image before filter: {}\n".format(image))
+
+        # Check for any files not wanting to be processed
+        if image.startswith("."):
+            continue
+
+        print("image after filter: {}\n".format(image))
+
+        # get old extension of images at ext
+        fn, ext = os.path.splitext(image)
+
+        print("fn: {}, ext: {}\n".format(fn, ext))
+
+        # get new file name with extension
+        newFn = "{}{}".format(fn, extension)
+
+        print("newFn: {}\n".format())
+
+        # Get full path to unprocessed image
+        unPath = os.path.join(dir, image)
+
+        print("unPath: {}\n".format(unPath))
+
+        # Get full path to processed image
+        prPath = os.path.join(newDir, newFn)
+
+        print("prPath: {}\n".format(prPath))
+
+        # create instance of image at path called im
+        im = Image(unPath)
+        im.rotate(rotate).resize(size).save(prPath))
+
+        # Unhash when debugging to go through images one by one
+        cont = "n"
+        while cont.lower().strip() != "y":
+            cont = input("Would you like to continue to the next file? ['y'/'n'] ...")
+
+def main():
+    dir, newDir, size, rotate, extension = sys.argv[1].split("|")
+
+    try:
+        os.path.isdir(dir)
+        os.path.isdir(newDir)
+        size = size.split(",")
+        size = (int(size[0]), int(size[1]))
+    except TypeError:
+        print("Please pass a directory to get started.")
+        print("Invocation: converter.py dir|newDir|size|rotate|ext")
+        sys.exit(1)
+    except Exception as e:
+        print("Please fix error in parameter format: {}".format(e))
+        sys.exit(101)
+
+    # Check to make sure dir and newDir exist
+    if os.path.isdir(dir) != True:
+        print("{} is not a vaid directory\nPlease pass a valid directory to get started".format(dir))
+        sys.exit(10)
+    if os.path.isdir(newDir) != True:
+        print("{} is not a vaid directory\nPlease pass a valid directory to get started".format(newDir))
+        sys.exit(11)
+
+    # Check to ensure size is a tuple and that it is not empty
+    if len(size) != 2 or type(size) != tuple:
+        if type(size) != tuple:
+            print("{} is not acceptable format\nPlease format size: |num1,num2|".format(size))
+            sys.exit(20)
+        print("size has length: {}\nPlease ensure size is a tuple formatted as: |num1,num2|".format(size))
+        sys.exit(21)
+
+    # Check to ensure rotate is an integer less than 360 or above 0
+    if type(rotate) != int or rotate >= 360 or rotate <= 0:
+        if type(rotate) != int:
+            print("{} is not an integer\nPlease input an integer for rotation".format(rotate))
+            sys.exit(30)
+        print("{} is above 360 or below 0\nPlease input a rotation less than 360 or above 0".format(rotate))
+        sys.exit(31)
+
+    # Check to ensure extension is a string that begins with (.)
+    if not extension.startswith(".") or type(extension) != str:
+        if type(extension) != str:
+            print("extension was type: {}\nPlease pass an extension of type str".format(extension))
+            sys.exit(40)
+        print("{} extension does not start with (.)\nPlease entr extension begining with (.)".format(extension))
+        sys.exit(41)
+
+    try:
+        convert(dir, newDir, size, rotate, extension)
+        sys.exit(0)
+    except Exception as e:
+        print("Error in convert(): {}".format(e))
+        sys.exit(101)
+
+if __name__ == "__main__":
+    main()
